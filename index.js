be7f4955d6262845fe87dbb86284777fc8c3409d
Carlos <carloscxo@gmail.com>
2022-09-01 15:15:22 +0200

    feat/Clean up children

--- index.js
+++ index.js
@@ -1,45 +1,43 @@
 const form = document.querySelector("#mainForm");
 const button = document.querySelector("#buttonGenerate")
 
-function generateRandomIntegerInRange(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
 
 function generateTitles (form) {
-    
+    /* Clean up */
+    const results = document.querySelector(".results");
+    while (results.firstChild)  {
+        results.removeChild(results.lastChild);
+    }
+
+    /* List of titles */
     const titles = [`Top 1 Apps Created With ${form.input.value} in 2022`,
     `Top 2 Apps Created With ${form.input.value} in 2022`,
     `Top 3 Apps Created With ${form.input.value} in 2022`]
 
-    let titlePicker1 = generateRandomIntegerInRange(0,titles.length-1);
+    /* Random generator */
+    function generateRandomIntegerInRange(min=0, max=titles.length-1) {
+        
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+            
+    }
     
-    let titlePicker2 = generateRandomIntegerInRange(0,titles.length-1);
-    
-    let titlePicker3 = generateRandomIntegerInRange(0,titles.length-1);
-
-
     if (form.input.value != "")  {
        let inputTech = form.input.value;
-       
-       let title1 = titles[titlePicker1];
-       document.getElementById("result1").innerHTML = title1
+    
+       /* Generate each title */
+        for (let index = 0; index <= 2; index++) {
 
-       let title2 = titles[titlePicker2];
-       document.getElementById("result2").innerHTML = title2
-
-       let title3 = titles[titlePicker3];
-       document.getElementById("result3").innerHTML = title3
+            let paragraph = document.createElement("p");
+            paragraph.setAttribute("id",`result${index}`);
+            paragraph.innerHTML = titles[generateRandomIntegerInRange()];
+            document.querySelector(".results").appendChild(paragraph)
+        }
 
    }
+
    else {
        alert ("You must type something!");
    }
-
-   console.log("titles.lenght = " + titles.length)
-   console.log("titlePicker = " + titlePicker1)
 } 
-
-
-
 
 button.addEventListener("click",()=>generateTitles(form))
