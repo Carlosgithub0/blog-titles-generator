d0fa6a75bb2da6243f59e537950185a143104ddc
Carlos <carloscxo@gmail.com>
2022-09-01 14:32:01 +0200

    feat/randomized sentence selection

--- index.js
+++ index.js
@@ -1,43 +1,45 @@
 const form = document.querySelector("#mainForm");
 const button = document.querySelector("#buttonGenerate")
 
-/* moved const out of function so titles can be found outside of it */
-const titles = [`Top 1 Apps Created With ${form.input.value} in 2022`,
-`Top 2 Apps Created With ${form.input.value} in 2022`,
-`Top 3 Apps Created With ${form.input.value} in 2022`]
-
-/*function to generate random number including min and max */
 function generateRandomIntegerInRange(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-let titlePicker = generateRandomIntegerInRange(0,titles.length);
-
 function generateTitles (form) {
     
+    const titles = [`Top 1 Apps Created With ${form.input.value} in 2022`,
+    `Top 2 Apps Created With ${form.input.value} in 2022`,
+    `Top 3 Apps Created With ${form.input.value} in 2022`]
+
+    let titlePicker1 = generateRandomIntegerInRange(0,titles.length-1);
+    
+    let titlePicker2 = generateRandomIntegerInRange(0,titles.length-1);
+    
+    let titlePicker3 = generateRandomIntegerInRange(0,titles.length-1);
+
+
     if (form.input.value != "")  {
-        /* updated name from test to inputTech */
        let inputTech = form.input.value;
        
-       /* old, safe version 
-       let title1 = `Top 5 Apps Created With ${inputTech} in 2022`
-       document.getElementById("result1").innerHTML = title1   
-       */
-       
-       /* ? -> Why doesn't it get the $inputTech? */
-       let title1 = titles[titlePicker]
+       let title1 = titles[titlePicker1];
        document.getElementById("result1").innerHTML = title1
+
+       let title2 = titles[titlePicker2];
+       document.getElementById("result2").innerHTML = title2
+
+       let title3 = titles[titlePicker3];
+       document.getElementById("result3").innerHTML = title3
 
    }
    else {
        alert ("You must type something!");
    }
 
+   console.log("titles.lenght = " + titles.length)
+   console.log("titlePicker = " + titlePicker1)
 } 
 
-/* checks */
-console.log("random " + titles.length)
-console.log("random " + titlePicker)
+
 
 
 button.addEventListener("click",()=>generateTitles(form))
