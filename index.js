edfd77a9c7ef051355c899ec52d6eea8479c28d7
Carlos <carloscxo@gmail.com>
2022-10-05 18:16:46 +0200

    feat/UX changed to flex

--- index.js
+++ index.js
@@ -8,7 +8,11 @@ const threat = document.querySelector("#inputThreat");
 
 function generateTitles(tech, industry, persona, hero, threat) {
   industry = industry || " In IT "; // TODO debug why it's not passing the default value
+  /* Fallbacks */;
+  hero.value.length == 0 ? hero.value = "Your Hero" : null;
+  threat.value.length == 0 ? threat.value = "Threat" : null;
 
+  
   // Cleans up on every submit + resets animations
   const results = document.querySelector(".results");
   while (results.firstChild) {
@@ -24,44 +28,53 @@ function generateTitles(tech, industry, persona, hero, threat) {
   tech.value = capitalizeFirstLetter(tech.value);
   industry.value = capitalizeFirstLetter(industry.value);
 
-  // Adds "In" to Industry if there's text there, but only once
-  if (industry.value.length === 0 || industry.value.includes("In ")) {
-    null;
-  } else {
-    industry.value = ` In ${industry.value}`;
-  }
-
   // List of titles
-  const titles1 = [
-    `Top ${genRanNum()} Apps Created With ${tech.value} in 2022${industry.value}`,
-    `How Safe Is Your ${tech.value} Stack from ${threat.value}${industry.value}?`,
-    `${genRanNum()} Warning Signs That Your ${tech.value} Project is in Danger${industry.value}`,
-    `Can We Really Trust ${tech.value} Developers${industry.value}?`,
-    `The Shocking Truth About ${tech.value} ${industry.value}`,
-    `Don't Gamble with Your ${tech.value} Project:  ${genRanNum()} Ways to Protect Yourself${industry.value}`,
-    `Lies ${tech.value} Developers Like to Tell${industry.value}`,
-    `${genRanNum()} Things Your ${tech.value} Developers Won’t Tell You${industry.value}`,
-    `${genRanNum()} Little-Known Factors That Could Affect Your ${tech.value} Project${industry.value}`,
-    `${tech.value} Alert: The New ${threat.value} Threat to Avoid${industry.value}`,
-    `Can’t Keep up? ${genRanNum()} Ways to Simplify Your ${tech.value} Workload`, 
+  const titleListTech = [
+    `Top ${genRanNum()} Apps Created With ${tech.value} in 2022 ${"In "+industry.value}`,
+    `${genRanNum()} Warning Signs That Your ${tech.value} Project is in Danger ${"In "+industry.value}`,
+    `${genRanNum()} Things Your ${tech.value} Developers Won’t Tell You ${"In "+industry.value}`,
+    `${genRanNum()} Little-Known Factors That Could Affect Your ${tech.value} Project ${"In "+industry.value}`,
+    `${tech.value} Alert: The New ${threat.value} to Avoid ${"In "+industry.value}`,
+    `Can’t Keep Up? ${genRanNum()} Ways to Simplify Your ${tech.value} Workload`, 
     `How to Take Charge of Your Day In ${tech.value}`,
     `${genRanNum()} Shortcuts for Completing Tedious ${tech.value} Tasks in Record Time`,
-    `Get Rid of this ${tech.value} ${threat.value} Risk${industry.value} Once and for All`,
-    `${genRanNum() * 5} Hacks: A Cheat Sheet for ${tech.value}${industry.value}`,
+    `${genRanNum() * 5} Hacks: A Cheat Sheet for ${tech.value} ${"In "+industry.value}`,
     `Use ${tech.value} Like ${hero.value}: ${genRanNum()} Ways`,
-    `${hero.value}’s Top ${genRanNum()} Tips for ${tech.value}${industry.value}`,
-    `${genRanNum()} Secrets of Microsoft ${tech.value} Developers${industry.value}`,
-    `What This Case Study Can Teach Us About ${tech.value}${industry.value}`,
-    `Do You Make These ${genRanNum()} ${tech.value} Mistakes${industry.value}?`,
-    `${genRanNum()} ${tech.value} Mistakes That Can Derail Your Project`,
+    `${hero.value+"'s"} Top ${genRanNum()} Tips for ${tech.value} ${"In "+industry.value}`,
+    `${genRanNum()} Secrets of Microsoft ${tech.value} Developers ${"In "+industry.value}`,
+    `Do You Make These ${genRanNum()} ${tech.value} Mistakes ${"In "+industry.value}?`,
     `${genRanNum()} ${tech.value} Mistakes That Make You Look Like a Newbie`,
   ];
-  const titles2 = [
-    `AAAAA`,
+
+  const titleListBusiness = [
+   `Get Rid of This ${tech.value} ${threat.value} Risk ${"In "+industry.value} Once and for All`,
+   `What This Case Study Can Teach Us About ${tech.value} ${"In "+industry.value}`,
+   `${genRanNum()} ${tech.value} Mistakes That Can Derail Your Project`,
+   `The Shocking Truth About ${tech.value}  ${"In "+industry.value}`,
+   `Don't Gamble with Your ${tech.value} Project:  ${genRanNum()} Ways to Protect Yourself ${"In "+industry.value}`,
+   `Can We Really Trust ${tech.value} Developers ${"In "+industry.value}?`,
+   `Lies ${tech.value} Developers Like to Tell ${"In "+industry.value}`,
+   `How Safe Is Your ${tech.value} Stack from ${threat.value} ${"In "+industry.value}?`,
+
   ];
 
+  // Choose title list based on persona picker
+  var titleListSelected = undefined;
+
+  switch (persona.value) {
+    case "0": titleListSelected = titleListBusiness;
+    console.log('case 0');
+        break;
+    case "1": titleListSelected = titleListBusiness.concat(titleListTech);
+    console.log('case 1');
+    break;
+    case "2": titleListSelected = titleListTech;
+        break;
+    default: break;
+}
+
   // Random number generator for titles
-  function generateRandomIntegerInRange(min = 0, max = titles1.length - 1) {
+  function generateRandomIntegerInRange(min = 0, max = titleListSelected.length - 1) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
 
@@ -80,18 +93,16 @@ function generateTitles(tech, industry, persona, hero, threat) {
     document.querySelector(".results").classList.add("fader")
     document.querySelector(".results").style.animationPlayState = "running";
 
-
     // Generate each title
     for (let index = 0; index <= 2; index++) {
-let number = generateRandomIntegerInRange();
+      let number = generateRandomIntegerInRange();
       let listNumber = genRanNum();
       let paragraph = document.createElement("p");
-      paragraph.innerHTML = `${titles1[number]} (${
-        titles1[number].length + inputTech.length
-      } chars)`;
+      paragraph.innerHTML = `${titleListSelected[number]} (${
+      titleListSelected[number].length + inputTech.length} chars)`;
       paragraph.setAttribute("id", `result${index}`);
       document.querySelector(".results").appendChild(paragraph);
-      titles1.splice(number, 1); // removes one index
+      titleListSelected.splice(number, 1); // removes one index
     }
   } else {
     // Validation for empty Technoloty field
@@ -100,8 +111,6 @@ let number = generateRandomIntegerInRange();
     void inputTechnology.offsetWidth;
     inputTechnology.classList.add("blinkMe");
     inputTechnology.style.animationPlayState = "running";
-
-    //OLD    alert ("You must write down a technology!"); // Tech can't be empty
 
   }
 }
